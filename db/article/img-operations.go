5977233c910178c63c0671df4cff48dfdd359645
alexpan <p.aleksey@do-it.co>
2018-01-17 13:59:47 +0200

    Implemented support for both jpg and png images

--- db/article/img-operations.go
+++ db/article/img-operations.go
@@ -4,7 +4,8 @@ import (
 	"bytes"
 	"encoding/base64"
 	"errors"
-	"fmt"
+	"image"
+	"image/jpeg"
 	"image/png"
 	"os"
 	"regexp"
@@ -13,30 +14,43 @@ import (
 	"time"
 )
 
-// TODO: make with go routines
 func SaveImage(text *string) (string, error) {
 	timeStr := strconv.FormatInt(time.Now().UnixNano(), 10)
-	imagePath := "public/images/" + timeStr + "-image.png"
-	fmt.Println(imagePath)
+	imagePath := "public/images/" + timeStr + "-image"
 
 	var finishedArticle string
 	re := regexp.MustCompile(`src\s*=\s*"data:image/(\w{3});base64,(.+?)"`)
 
-	image := re.FindStringSubmatch(*text)
-	if len(image) == 0 {
+	srcAttr := re.FindStringSubmatch(*text)
+	if len(srcAttr) == 0 {
 		return *text, nil
 	}
 
-	base64Str := image[2]
+	fileType := srcAttr[1]
+	base64Str := srcAttr[2]
 	unbased, err := base64.StdEncoding.DecodeString(base64Str)
 	if err != nil {
 		return "", errors.New("Cannot decode base64 to img")
 	}
 
 	r := bytes.NewReader(unbased)
-	img, imgErr := png.Decode(r)
-	if imgErr != nil {
-		return "", imgErr
+	var img image.Image
+	var imgErr error
+	// decode regarding image type
+	if fileType == "png" {
+		imagePath += ".png"
+		img, imgErr = png.Decode(r)
+		if imgErr != nil {
+			return "", imgErr
+		}
+	} else if fileType == "jpg" {
+		imagePath += ".jpg"
+		img, imgErr = jpeg.Decode(r)
+		if imgErr != nil {
+			return "", imgErr
+		}
+	} else {
+		return "", errors.New("Wrong image type. Use .jpg or .png")
 	}
 
 	f, fileErr := os.Create(imagePath)
@@ -46,7 +60,7 @@ func SaveImage(text *string) (string, error) {
 
 	png.Encode(f, img)
 	savedImagePath := "src=\"/" + imagePath + "\""
-	finishedArticle = strings.Replace(*text, image[0], savedImagePath, 1)
+	finishedArticle = strings.Replace(*text, srcAttr[0], savedImagePath, 1)
 
 	return SaveImage(&finishedArticle)
 }
